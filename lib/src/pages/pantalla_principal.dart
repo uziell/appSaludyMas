8f9c2a4ef745fe8582f66398ac017c328e2178b8
uziell <uziell_sanchez@outlook.com>
2021-09-27 18:03:59 -0500

    ya quedo listo el dropmenu y el carrucel de imagenes

--- lib/src/pages/pantalla_principal.dart
+++ lib/src/pages/pantalla_principal.dart
@@ -6,6 +6,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:salud_y_mas/src/models/modelo_categoria.dart';
 
 
+
 class PantallaPrincipal extends StatefulWidget {
   @override
   _PantallaPrincipalState createState() => _PantallaPrincipalState();
@@ -13,36 +14,30 @@ class PantallaPrincipal extends StatefulWidget {
 class _PantallaPrincipalState extends State<PantallaPrincipal> {
 
   List<String> estadosConsulta = [], ciudadesConsulta = [], imagenes =[];
-  String _estadoActual = '', _ciudadActual = "";
-  List<DropdownMenuItem<String>> _dropDownMenuEstados = [], _dropDownMenuCiudades = [];  
   List<MyModel> myData = [];
   String urlApi = 'https://www.salumas.com/Salud_Y_Mas_Api/';
-  List<dynamic> listImagenes = [] ;
-
+  List<String> listImagenes = [] ;
+  String vista = 'Seleccione';
+  String vistaCiudad = "Seleccione una ciudad";
 
 
   Future consultarImagenes() async {
     final url = Uri.parse(urlApi+'consultas_carrusel.php');
     var response = await http.get(url);
     var respuestaImagen = jsonDecode(response.body);
-    
     for (var valor in respuestaImagen) {
       imagenes.add((valor['imagen']).toString());
     }
      print('Esto tiene imagenes: '+ imagenes.toString());
   }
-
-  
   llenarCarrucel(List<String> lista) {
     print('Esto tiene lista:'+lista.toString());
     for (var i = 0; i < lista.length; i++) {
-       listImagenes.add(Uri.parse(urlApi+'/images/'+lista[i]));
+       listImagenes.add(urlApi+'/images/'+lista[i]);
     }
   }
 
-  
   Future consultarAPIEstados() async {
-
     final url = Uri.parse(urlApi+'consultas_edo.php');
     var response = await http.get(url);
     var respuesta = jsonDecode(response.body);
@@ -51,54 +46,23 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
       estadosConsulta.add((valor['nombre']));
     }
   }
-  List<DropdownMenuItem<String>> contruirDropEstados() {
-    List<DropdownMenuItem<String>> items = [];
-
-    for (String estado in estadosConsulta) {
-      items.add(
-          DropdownMenuItem(
-            child: Text(estado),
-            value:  estado,
-          )
-      );
-
-      print('Esto tiene estados: '+ estado);
-      print('Este es un estado nuevo'+estado);
-    }
-    return items;
-  }
 
   Future consultarAPICiudades(String estadoCiu) async {
-    _dropDownMenuCiudades.clear();
     ciudadesConsulta.clear();
-    _ciudadActual = '';
+   // _ciudadActual = '';
     final url = Uri.parse(urlApi+'consultas_cd?nameEdo='+estadoCiu);
     var response = await http.get(url);
     var respuesta = jsonDecode(response.body);
 
     //print(respuesta);
     for (var valor in respuesta) {
-      ciudadesConsulta.add((valor['nombre']));
-      
+      ciudadesConsulta.add((valor['nombre'])); 
     }
-    
+     print('item: '+ciudadesConsulta.toString());
     if(ciudadesConsulta.isEmpty){
       return;
     }
-   List<DropdownMenuItem<String>> itemsC = [];
-    for (String ciudades in ciudadesConsulta) {
-      itemsC.add(
-          DropdownMenuItem(
-                value: ciudades,
-                child: 
-                    Text(ciudades,overflow: TextOverflow.ellipsis,
-                    )
-          ),         
-      ); 
-      _dropDownMenuCiudades = itemsC;
-      print('Esto tiene ciudades: '+ ciudades);
-     // print('item: '+itemsC.toString());
-    }     
+
   }
 
   @override
@@ -108,21 +72,22 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
        llenarCarrucel(imagenes);
        
     });
-   
     consultarAPIEstados().then((resultado) {
       setState(() {
-        _dropDownMenuEstados = contruirDropEstados();
-        _estadoActual = _dropDownMenuEstados[0].value.toString();
+       // _dropDownMenuEstados = contruirDropEstados();
+       // _estadoActual = _dropDownMenuEstados[0].value.toString();
+        
         
       });
     });
   }
   
-  Future consultarCategorias(String actualStado)async{
+  Future consultarCategorias(String actualStado, String actualCiudad)async{
     myData.clear();
-    final  urlApi = Uri.parse('https://www.salumas.com/Salud_Y_Mas_Api/consultas_categorias?nameEdo='+ actualStado);
+    print('Esto tiene ciudadActual '+ actualCiudad);
+    final  urlApi = Uri.parse("https://www.salumas.com/Salud_Y_Mas_Api/consultas_categorias?nameEdo="+actualStado+"&nameCd="+actualCiudad);
+    print(urlApi);
     var response = await http.get(urlApi);
-
     if(response.statusCode == 200){
       String responseBody = response.body;
       var jsonBody =   json.decode(responseBody);
@@ -141,113 +106,115 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        
-        appBar: AppBar(
+        extendBodyBehindAppBar: true,
+         appBar: AppBar(
           title: Text('Salud y Mas'),
+
         ),
-        body: ListView(
-                  children: [
-                    llamardropDwownMenu(),
-                    Divider(),
-                    _llenarCarrucel(),
-                  // llenarCarrucelPrueba(),
-                    Divider(),
-                    _llamarCategorias(),
-                  ],
-                ),
+        body: Column(         
+        children: [
+        Padding(padding: EdgeInsetsDirectional.all(35)),
+        llamarDropPrueba(),
+         //este es el buen carrucelPrueba
+         llenarCarrucelPrueba(),
+        
+        ],
+        ),
             ),
         );
   }
 
-  llamardropDwownMenu(){
-    return Column(
-      children: [
-        Row(
-          children: [
-            DropdownButton(
-              hint: Text('Seleccion'),
-              value: _estadoActual,
-              items: _dropDownMenuEstados,
-              onChanged: (String? seleccion){
-                setState(() {
-                  _estadoActual = seleccion!;
-                  consultarAPICiudades(_estadoActual).then((value){
-                    setState(() {
-                      _ciudadActual = _dropDownMenuCiudades[0].value.toString();
-                    });
-                  });
-                  consultarCategorias(_estadoActual).then((value) {
-                   // llenarImagenesEspecialidad(imagenesCategoria);
-                  });
-                });
-              },
-            ),
-            SizedBox(
-              width: 5.0,
-            ),
-            DropdownButton(
-              hint: Text('Seleccionar Ciudad'),
-              value: _ciudadActual,
-              items: _dropDownMenuCiudades,
-              onChanged: (String? seleccion){
-                setState(() {
-                  _ciudadActual = seleccion!;
-                });
-              },
-            )
-          ],
-        )
-      ],
-    );
-
-  }
-
-  _llenarCarrucel() {
-    CarouselController buttonCarouselController = CarouselController();
-    return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children:<Widget> [
-          CarouselSlider(
-            items: listImagenes.map((imgS){
-              return Builder(
-                builder: (BuildContext context){
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white
+  llamarDropPrueba(){
+    return Row(
+            children: [
+                 Container(
+                  width: 150,
+                  padding: EdgeInsets.symmetric(horizontal:12, vertical: 4),
+                   decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.black,width: 2)
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                      isExpanded: true,
+                      iconSize: 20,
+                      icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                      items: estadosConsulta.map((String estadoC) {
+                        return DropdownMenuItem(
+                          value: estadoC,
+                          child: Text(estadoC, 
+                          ),
+                          );
+                      }).toList(),
+                      onChanged: (String? seleccion){
+                        setState(() {
+                          vista = seleccion.toString();
+                          consultarAPICiudades(vista).then((value){
+                          setState(() {
+                            vistaCiudad = ciudadesConsulta[0].toString();
+                           
+                          });
+                        });
+                      });
+                    },
+                      hint: Text(vista, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.network(imgS.toString()),
-                    ),
-                  );
-                },
-              );
-            }).toList(),
-            carouselController: buttonCarouselController ,
-            options: CarouselOptions(
-              height: 250,
-              aspectRatio: 16/9,
-              viewportFraction: 0.8,
-              initialPage: 0,
-              enableInfiniteScroll: true,
-              reverse: false,
-              autoPlay: true,
-              //autoPlayAnimationDuration: Duration(microseconds: 3),
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayCurve: Curves.fastOutSlowIn,
-              scrollDirection: Axis.horizontal,
+                  ),
+                ),   
+              SizedBox(
+                width: 5,
+              ),
+              Container(
+                  width: 250,
+                  padding: EdgeInsets.symmetric(horizontal:12, vertical: 4),
+                   decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.black,width: 2)
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                      items: ciudadesConsulta.map((String ciudadC) {
+                        return DropdownMenuItem(
+                          value: ciudadC,
+                          child: Text(ciudadC),
+                          );
+                      }).toList(),
+                      iconSize: 20,
+                      isExpanded: true,
+                      icon: Icon(Icons.arrow_drop_down, color: Colors.black),
+                      onChanged: (_ciudadActual){
+                        setState(() {
+                          vistaCiudad = _ciudadActual.toString();
+                           _llamarCategorias(vistaCiudad);
+                          });
+                      },
+                      hint: Text(vistaCiudad,style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+                      ),
+                  ),
+                ),
               
-            ),
-            
-          )
-        ],
-      ),
-    );
+            ],
+          );
+    
   }
-
-  _llamarCategorias() {
+llenarCarrucelPrueba() {
+return Container(
+  width: double.infinity,
+  height: 250.0,
+  child: Swiper(
+    
+        itemBuilder: (BuildContext context,int index){
+          return new Image.network(listImagenes[index],fit: BoxFit.fill,);
+        },
+        itemCount: listImagenes.length,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+        autoplay: true,
+      ),
+);
+ 
+}
+  _llamarCategorias(String nombreCiudad) {
    final uriApss = Uri.parse(urlApi+'images/especialidades/default.png');
     var size = MediaQuery.of(context).size;
     final double itemHeight = size.height * 15.8;
@@ -308,6 +275,11 @@ class _PantallaPrincipalState extends State<PantallaPrincipal> {
     );
     
   }
+
+  
+
+  
+
 
   
 
