23866cbab5f045f8bd4738abe95ae1496b754330
Hugo Jair Quintal Cervantes <hugo.quintal.cervantes@gmail.com>
2022-01-22 14:16:53 -0500

    Se le puso los :

--- lib/apps/ui/pages/home/home_page.dart
+++ lib/apps/ui/pages/home/home_page.dart
@@ -67,7 +67,7 @@ class HomePageGoogle extends StatelessWidget {
                                                                 FontWeight
                                                                     .bold)),
                                                     Text(
-                                                        'Tiempo aproximado: ${controller.tiempo.toStringAsFixed(2)} ${controller.tiempo >= 60 ? 'hrs' : 'min'}',
+                                                        'Tiempo aproximado: ${controller.tiempoLabel} ${controller.tiempo >= 60 ? 'hrs' : 'min'}',
                                                         style: TextStyle(
                                                             fontSize: 16,
                                                             fontWeight:
